df867ba200ea92687637d012d9562961b2d01358
aaaa <admin@admin.com>
2018-06-14 19:12:58 +0800

    圆圈生成及碰撞检测

---
+++ algo_project/src/algo_project/AlogoVisualizer.java
@@ -0,0 +1,77 @@
+package algo_project;
+
+import java.awt.EventQueue;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class AlogoVisualizer {
+
+	private Circle[] circles;
+	private AlgoFrame frame;
+	private boolean isAnimated = true;
+	
+	public AlogoVisualizer(int sceneWidth, int sceneHeight, int N)
+	{
+		//初始化数据
+		circles = new Circle[N];
+		int R = 50;
+		for(int i = 0; i < N; i++)
+		{
+			int x = (int)(Math.random()*(sceneWidth-2*R)) +R;
+			int y = (int)(Math.random()*(sceneHeight-2*R)) +R;
+			int vx = (int)(Math.random()*11) -5;
+			int vy = (int)(Math.random()*11) -5;
+			
+			circles[i] = new Circle(x, y, R, vx, vy);
+		}
+		
+		//初始化视图
+		//将GUI创建窗口放入新的线程（java的事件分发线程）
+		EventQueue.invokeLater(() -> {
+			frame = new AlgoFrame("Welcome", sceneWidth, sceneHeight);
+			frame.addKeyListener(new AlgoKeyListener());
+			new Thread(() -> {
+				run();
+			}).start();
+			
+		});
+				
+	}
+	
+	private void run()
+	{
+		while(true)
+		{
+			//绘制数据
+			frame.render(circles);
+			AlgoVisHelper.pause(20);
+			//更新数据
+			if(isAnimated)
+				for(Circle circle : circles)
+					circle.move(0, 0, frame.getCanvasWidth(), frame.getCanvasHeight());
+		}
+	}
+	
+	private class AlgoKeyListener extends KeyAdapter
+	{
+		@Override
+		public void keyReleased(KeyEvent event)
+		{
+			if(event.getKeyChar() == ' ')
+				isAnimated = !isAnimated;
+		}
+	}
+	
+	//主函数
+	public static void main(String[] args)
+	{
+			
+			int sceneWidth = 800;
+			int sceneHeight = 800;
+			int N = 10;
+			
+			AlogoVisualizer visualizer = new AlogoVisualizer(sceneWidth, sceneHeight, N);
+			
+	}
+}
